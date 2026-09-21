37f7c0fd917cf0819f4f508832d852e86b5390ad
Nicolle <nicolmera391@gmail.com>
2026-09-21 12:18:36 -0500

    Se creo la clase de inventario

--- src/Model/Inventario.java
+++ src/Model/Inventario.java
@@ -1,5 +1,50 @@
 package Model;
 
 public class Inventario {
-    
+
+    private String codigo;
+    private String nombreProducto;
+    private int stock;
+    private double precio;
+
+    public Inventario() {}
+
+    public Inventario(String codigo, String nombreProducto, int stock, double precio) {
+        this.codigo = codigo;
+        this.nombreProducto = nombreProducto;
+        this.stock = stock;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
